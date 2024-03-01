dbefc1463a8016ac6981c50b4a43957ed97e1bd7
Ethrusk <ethrusk@yandex.ru>
2024-03-01 12:44:10 +0300

    programm 1st version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите элементы массива через пробел:");
+        string? input = Console.ReadLine();
+        string[] inputArray = input!.Split( );
+
+        string[] resultArray = new string[inputArray.Length];
+        int resultIndex = 0;
+
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                resultArray[resultIndex] = str;
+                resultIndex++;
+            }
+        }
+
+        Array.Resize(ref resultArray, resultIndex);
+
+        Console.WriteLine("Результат:");
+        foreach (string str in resultArray)
+        {
+            Console.WriteLine(str);
+        }
